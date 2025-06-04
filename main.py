844261f3c0e4c901b528409031dfa5269c79a649
tb-kirby <blukirby@gmail.com>
2025-06-03 20:50:04 -0700

    added 60fps limit to main loop, created circleshape class

--- main.py
+++ main.py
@@ -10,12 +10,18 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("Black")
         pygame.display.flip()
+        
+        dt = clock.tick(60)/1000
         
         
 
