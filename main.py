ba076c4b9b0317bcf386795f5c219f05ce3a21ae
tb-kirby <blukirby@gmail.com>
2025-06-05 16:47:17 -0700

    added player turning and movement

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -14,15 +15,17 @@ def main():
 
     dt = 0
 
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("Black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         
         dt = clock.tick(60)/1000
-        
         
 
 if __name__ == "__main__":
